4b7f8dce6acc61094ad01c1d68f45f6cf760bbbc
cao-uy <caovanuy00@gmail.com>
2023-08-16 09:53:31 +0700

    4.Chuyen doi kieu du kieu - Tryparse

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,8 +3,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-    string str = "123";
-    int a = int.Parse(str);
-    System.Console.WriteLine("a = " + a);
+    string str = "97979"; //gia tri dau vao de chuyen doi
+    int ketQua; //bien luu ket qua sau chuyen doi
+    //bien kiem tra chuyen doi co thanh cong hay khong
+    bool kiemTra = false;
+    //chuyen doi kieu du lieu
+    kiemTra = int.TryParse(str, out(ketQua));
+    System.Console.WriteLine("Ket qua = " + ketQua);
     }
 }
