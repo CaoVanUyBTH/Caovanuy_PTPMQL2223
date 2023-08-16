e18778e4f2c59aa531b134da25b9a2f00287368a
cao-uy <caovanuy00@gmail.com>
2023-08-16 09:40:52 +0700

    TH Bien So const

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,9 +4,9 @@ internal class Program
     private static void Main(string[] args)
     {
       //Khai bao bien kieu string va gan gia tri = Pham Van Dong
-      string diachi = "Pham Van Dong";
+      const string diachi = "Pham Van Dong";
       //Khai bao bien kieu in va gan gia tri = 18
-      int sodienthoai = 0915544260;
-      Console.WriteLine("Dia chi {0} - So dien thoai {1} ", diachi, sodienthoai);
+      const long sodienthoai = 0915544260;
+      Console.WriteLine("Dia chi {0} - So dien thoai {1}", diachi, sodienthoai);
     }
 }
