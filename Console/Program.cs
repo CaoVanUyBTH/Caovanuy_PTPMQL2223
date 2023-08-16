0074dcef7c4f3359e6bfcd170c282380d09d38c1
cao-uy <caovanuy00@gmail.com>
2023-08-16 10:12:46 +0700

    5. Cac toan tu - chia lay nguyen va lay du

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,10 +3,16 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-    string str = "97979"; //gia tri dau vao de chuyen doi
-   //chuyen doi kieu du lieu string (gia tri = "97979")
-   //sang kieu du lieu int (gia tri sau khi chuyen doi = 97979)
-    int a = Convert.ToInt32(str);
-    System.Console.WriteLine("a = " + a);
+        int a, b; //Khai bao 2 bien so a va b kieu int
+        //thong bao cho nguoi dung nhap vao a
+        System.Console.Write("a = ");
+        //doc du lieu nhap tu ban phim chuyen snag kieu int va gan cho a
+        a = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("b = ");
+        //doc du lieu nhap tu ban phim chuyen snag kieu int va gan cho b
+        b = Convert.ToInt32(Console.ReadLine());
+        //in tong a + b ra ngoai man hinh
+        System.Console.WriteLine("{0} / {1} = {2}", a, b, a/b);
+        System.Console.WriteLine("{0} % {1} = {2}", a, b, a%b);
     }
 }
