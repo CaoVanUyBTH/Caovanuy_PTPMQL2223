3f8b4d6c1063ad393cd1f9d0645e85e5ad9db14d
cao-uy <caovanuy00@gmail.com>
2023-08-16 09:47:21 +0700

    4.Chuyen doi du lieu

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,10 +3,8 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-      //Khai bao bien kieu string va gan gia tri = Pham Van Dong
-      const string diachi = "Pham Van Dong";
-      //Khai bao bien kieu in va gan gia tri = 18
-      const long sodienthoai = 0915544260;
-      Console.WriteLine("Dia chi {0} - So dien thoai {1}", diachi, sodienthoai);
+    string str = "123";
+    int a = int.Parse(str);
+    System.Console.WriteLine("a = " + a);
     }
 }
