adae840577ed1358c9d1de5674af7f22abd6da90
cao-uy <caovanuy00@gmail.com>
2023-08-29 11:34:31 +0700

    Mang

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,16 +1,20 @@
-﻿using System.Security.Cryptography;
-using Console.Models;
+﻿using Console.Test.Test;
 public class Program
 {
-    private static void Main (string[] args)
+    private static void Main(string[] args)
     {
-        // Person ps1 = new Person();
-        // Person ps2 = new Person();
-        // ps1.FullName = "Nguyen Van A";
-        // ps1.Address = "Ha Noi";
-        // ps1.Age = 18;
-        // ps1.Display();
-        System.Console.WriteLine("A = ");
-        int a =  Convert.ToInt32(Console.ReadLine());
+        int n = 0;
+        do
+        {
+            Console.WriteLine("n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+        } while (n < 1);
+        Test [] TestArray = new Test[n];
+        for (int i = 0; i <TestArray.Length; i++)
+        {
+            Test ts = new Test();
+            ts.EnterData();
+            TestArray[i] = ts;
+        }
     }
 }
