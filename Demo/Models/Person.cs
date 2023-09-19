fd8df94e54eddd29406b187e91ca7a14bb3abe30
cao-uy <caovanuy00@gmail.com>
2023-09-19 10:48:00 +0700

    Tao Person trong models

---
+++ Demo/Models/Person.cs
@@ -0,0 +1,10 @@
+namespace Demo.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set;}
+        public string PersonName { get; set;}
+        public string PersonAddress { get; set;}
+
+    }
+}
