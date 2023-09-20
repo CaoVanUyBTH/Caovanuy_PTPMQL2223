746281cd4749d6c29ac4fa85ff350aa5275b5b54
cao-uy <caovanuy00@gmail.com>
2023-09-20 09:11:49 +0700

    Tao moi MvcMovie va lam viec voi controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        //GET:/HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
